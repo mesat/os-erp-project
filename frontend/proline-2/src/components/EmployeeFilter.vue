ddd6995e3e3fcdc142526a2098080cd86edbaf7b
Ayçe Gülüm Saltık <aycegsaltk@outlook.com>
2021-08-02 15:00:08 +0300

    Filter eklendi.

--- frontend/proline-2/src/components/EmployeeFilter.vue
+++ frontend/proline-2/src/components/EmployeeFilter.vue
@@ -2,10 +2,44 @@
 
   <CCard>
     <CCardHeader>
-      <CToggler inHeader="true" @click="toggleFilter"><CIcon name="cilList" /></CToggler>
+      <CToggler inHeader="true" @click="toggleFilter">
+        <CIcon name="cilList"/>
+      </CToggler>
     </CCardHeader>
     <CCollapse :show="filterShow">
-      lorem ipsum
+      <CCardBody>
+        <CRow>
+          <CCol>
+            <CInput name="name" label="Name" placeholder="enter name"/>
+          </CCol>
+          <CCol>
+            <CInput name="last name" label="Surname" placeholder="enter surname"/>
+          </CCol>
+          <CRow>
+            <CCol>
+              <CInput name="telephone" label="Telephone" placeholder="enter phone number"/>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol>
+              <CInput name="date" label="Date" type="date"/>
+            </CCol>
+            <CCol>
+              <CInput name="date" label="..." type="date"/>
+            </CCol>
+          </CRow>
+        </CRow>
+      </CCardBody>
+      <CCardFooter>
+        <CRow>
+          <CCol col="10"/>
+          <CCol col="2">
+            <CButton color="success">
+              filter
+            </CButton>
+          </CCol>
+        </CRow>
+      </CCardFooter>
     </CCollapse>
   </CCard>
 </template>
@@ -13,14 +47,14 @@
 <script>
 export default {
   name: "EmployeeFilter",
-  data () {
+  data() {
     return {
       filterShow: true
     }
   },
   methods: {
-    toggleFilter () {
-      if(this.filterShow) {
+    toggleFilter() {
+      if (this.filterShow) {
         this.filterShow = false
       } else {
         this.filterShow = true
