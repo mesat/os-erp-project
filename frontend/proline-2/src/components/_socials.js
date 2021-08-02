fc20c07581d3a4878b4fb0119ef9e25ca338016a
Ayçe Gülüm Saltık <aycegsaltk@outlook.com>
2021-08-02 12:10:23 +0300

    Yeni sosyal medya eklendi.

--- frontend/proline-2/src/components/_socials.js
+++ frontend/proline-2/src/components/_socials.js
@@ -1,5 +1,10 @@
 export default [
     {name: 'Facebook', icon: 'cibFacebook'},
     {name: 'Twitter', icon: 'cibTwitter'},
-    {name: 'Linkedin', icon: 'cibLinkedin'}
+    {name: 'Linkedin', icon: 'cibLinkedin'},
+    {name: 'Instagram', icon: 'cibInstagram'},
+    {name: 'Github', icon: 'cibGithub'},
+    {name: 'Youtube', icon: 'cibYoutube'},
+    {name: 'Reddit', icon: 'cibReddit'},
+    {name: 'Pinterest', icon: 'cibPinterest'},
 ]
