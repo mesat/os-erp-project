830a93d82a05671d79439292426aaaf18cd35437
BuildTools <unconfigured@null.spigotmc.org>
2021-08-02 16:24:45 +0300

    employee in

--- frontend/proline-2/src/views/EmployeeView.vue
+++ frontend/proline-2/src/views/EmployeeView.vue
@@ -3,9 +3,15 @@
 </template>
 
 <script>
+import emp from "./_employee";
 export default {
   name: "EmployeeView",
-  props: ['item']
+  props: ['item'],
+  data () {
+    return {
+      item: emp
+    }
+  }
 }
 </script>
 
