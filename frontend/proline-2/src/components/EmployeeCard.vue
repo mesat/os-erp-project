8f1144a67cb1fffb7d9f5a1cc521e310dd05ad94
mbsoylu <soylumervebeyza@gmail.com>
2021-08-02 15:10:42 +0300

    fotoğraf eklendi bilgiler girildi

--- frontend/proline-2/src/components/EmployeeCard.vue
+++ frontend/proline-2/src/components/EmployeeCard.vue
@@ -1,5 +1,32 @@
 <template>
-<div/>
+  <CCard align="left">
+    <CCardHeader>
+      <img src="../assets/icons/admin.jpg" hegiht="150" width="150">
+    </CCardHeader>
+    <CCardBody>
+      <CRow>
+        <CCol>
+          <CCardText align="center">
+            <h1> AHMET </h1>
+          </CCardText>
+        </CCol>
+      </CRow>
+      <CRow>
+        <CCol>
+          <CCardText align="center">
+            <h2>SOYLU</h2>
+          </CCardText>
+        </CCol>
+      </CRow>
+      <CRow>
+        <CCol>
+          <CCardText align="center">
+            <h3>CIO</h3>
+          </CCardText>
+        </CCol>
+      </CRow>
+    </CCardBody>
+  </CCard>
 </template>
 
 <script>
