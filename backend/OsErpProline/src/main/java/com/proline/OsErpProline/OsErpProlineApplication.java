e320ebe95bea0bf1f326fd1c0348f8a022b58773
kaymakci <88039292+kaymakci@users.noreply.github.com>
2021-08-02 09:52:14 +0300

    Add files via upload
    init

---
+++ backend/OsErpProline/src/main/java/com/proline/OsErpProline/OsErpProlineApplication.java
@@ -0,0 +1,13 @@
+package com.proline.OsErpProline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsErpProlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsErpProlineApplication.class, args);
+	}
+
+}
