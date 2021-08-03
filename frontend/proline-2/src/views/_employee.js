23d8dbbbbc7a79c14bfd44d0affc49aaffb1ebf8
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 13:30:29 +0300

    test fixes

--- frontend/proline-2/src/views/_employee.js
+++ frontend/proline-2/src/views/_employee.js
@@ -5,6 +5,8 @@ export default [{
     date: "",
     role: "Cool Guy",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    telephone: "84103128031283",
+    mail: "jjjjj@benmail.co"
 }, {
     id: 1,
     name: "Bambam",
@@ -12,6 +14,8 @@ export default [{
     date: "",
     role: "Wrestler",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    telephone: "84103128031283",
+    mail: "jjjjj@benmail.co"
 }, {
     id: 2,
     name: "Rhett",
@@ -19,6 +23,8 @@ export default [{
     date: "",
     role: "Head of mythical mornings",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    telephone: "84103128031283",
+    mail: "jjjjj@benmail.co"
 }, {
     id: 3,
 
@@ -27,6 +33,8 @@ export default [{
     date: "",
     role: "Tv actor in 90s",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    telephone: "84103128031283",
+    mail: "jjjjj@benmail.co"
 }, {
     id: 4,
 
@@ -35,4 +43,6 @@ export default [{
     date: "",
     role: "Normal dude",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    telephone: "84103128031283",
+    mail: "jjjjj@benmail.co"
 }]
