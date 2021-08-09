dfdf5055c52c1ff5c8b54a21a86527359b8c9d53
Muhammet Esat Sakarya <esat.sakarya@pro-line.com.tr>
2021-08-09 11:31:56 +0300

    no message

--- frontend/proline-2/src/views/NewEmployee.vue
+++ frontend/proline-2/src/views/NewEmployee.vue
@@ -13,23 +13,63 @@
         <CCol md="9" class="order-md-0">
           <CRow>
             <CCol>
-              <CInput name="name" label="Name" placeholder="Enter employees name" v-model="name"/>
+              <CInput
+                  name="name"
+                  label="Name"
+                  placeholder="Enter employees name"
+                  :invalidFeedback= "message.name"
+                  :isValid="isValidName"
+                  v-model="name"
+                  lazy="false"
+              />
             </CCol>
             <CCol>
-              <CInput name="last-name" label="Surname" placeholder="Enter employees surname" v-model="surName"/>
+              <CInput
+                  name="last-name"
+                  label="Surname"
+                  placeholder="Enter employees surname"
+                  invalidFeedback= ""
+                  tooltipFeedback="true"
+                  :isValid = "isValidName"
+                  v-model="surName"
+              />
             </CCol>
             <CRow>
               <CCol>
-                <CInput label="Date" type="date" v-model="date"/>
+                <CInput
+                    label="Date"
+                    type="date"
+                    v-model="date"
+                    invalidFeedback= "message.date"
+                    tooltipFeedback="true"
+                    :isValid = "isValidDate"
+                />
               </CCol>
             </CRow>
           </CRow>
           <CRow md="2">
-            <CInput name="role" label="Role" placeholder="Role in the company" v-model="role"/>
+            <CInput
+                name="role"
+                label="Role"
+                placeholder="Role in the company"
+                invalidFeedback= ""
+                tooltipFeedback="true"
+                :isValid = "isValidRole"
+                v-model="role"
+            />
           </CRow>
           <CRow>
             <CCol>
-              <CTextarea name="bio" label="Bio" rows="8" placeholder="Short bio of the employee..." v-model="bio"/>
+              <CTextarea
+                  name="bio"
+                  label="Bio"
+                  rows="8"
+                  placeholder="Short bio of the employee..."
+                  invalidFeedback= ""
+                  tooltipFeedback="true"
+                  :isValid = "isValidBio"
+                  v-model="bio"
+              />
             </CCol>
           </CRow>
         </CCol>
@@ -42,8 +82,24 @@
       <h2>Communication Info</h2>
     </CCardHeader>
     <CCardBody>
-      <CInput name="tel" label="Telephone Number" type="mail" placeholder="Telephone number"/>
-      <CInput name="e-mail" label="E-Mail" type="tel" placeholder="Enter mail"/>
+      <CInput
+          name="tel"
+          label="Telephone Number"
+          type="mail"
+          placeholder="Telephone number"
+          :invalidFeedback= "message.tel"
+          tooltipFeedback="true"
+          :isValid = "isValidTel"
+      />
+      <CInput
+          name="e-mail"
+          label="E-Mail"
+          type="tel"
+          placeholder="Enter mail"
+          invalidFeedback= ""
+          tooltipFeedback="true"
+          :isValid = "isValidMail"
+      />
     </CCardBody>
     <CCardHeader>
       <strong>Social media</strong>
@@ -65,6 +121,25 @@
 <script>
 import Socials from "../components/Socials";
 
+const nameRegex = /^[A-Za-z]+$/i
+const nameMsg = "Names can only have letters and cannot be empty."
+
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+const emailMsg = "This is not a valid e-mail."
+
+const bioRegex = /^.+$/i
+const bioMsg = "This field cannot be empty."
+
+const dateRegex = /^.+$/i
+const dateMsg = "This field cannot be empty."
+
+const roleRegex = /^.+$/i
+const roleMsg = "This field cannot be empty."
+
+const telRegex = /^(\+[0-9]{12}|[0-9]{11})$/i
+const telMsg = "This is not a valid telephone"
+
+
 export default {
   name: "NewEmployee",
   data() {
@@ -74,13 +149,49 @@ export default {
       date: "",
       role: "",
       bio: "",
-      socials: []
+      socials: [],
+      valid: {},
+      message: {}
     }
   },
+  created() {
+    this.message.name= nameMsg
+    this.message.surName = nameMsg
+    this.message.date = dateMsg
+    this.message.role = roleMsg
+    this.message.bio = bioMsg
+    this.message.mail = emailMsg
+    this.message.tel = telMsg
+  },
   methods: {
-    submit () {
-      alert(this.date)
-    }
+    submit() {
+
+    },
+    isValid(val, regex) {
+      if(val === '' || val === undefined)
+      {
+        return null
+      }
+      else return val.match(regex) !== null;
+    },
+    isValidDate(val) {
+      return this.isValid(val, dateRegex)
+    },
+    isValidRole(val) {
+      return this.isValid(val, roleRegex)
+    },
+    isValidBio(val) {
+      return this.isValid(val, bioRegex)
+    },
+    isValidMail(val) {
+      return this.isValid(val, emailRegex)
+    },
+    isValidTel(val) {
+      return this.isValid(val, telRegex)
+    },
+    isValidName(val) {
+      return this.isValid(val, nameRegex)
+    },
   },
   components: {
     Socials
---
+++ frontend/proline-2/src/views/VueTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <CButton color="success" @click="click">
+      test request
+    </CButton>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  name: "VueTest",
+  data ()  {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    click(){
+      axios.get('https://os-erp.herokuapp.com/employees')
+      .then(function (response){
+        alert('a')
+        alert(response)
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
