ed841bfc0e73db1b41f7a919b533a60ed4a123cf
alperen010010 <bircakalperen@gmail.com>
2021-08-13 10:07:52 +0300

    Social media icon selection

--- frontend/proline-2/src/views/CreateSocial.vue
+++ frontend/proline-2/src/views/CreateSocial.vue
@@ -1,13 +1,75 @@
 <template>
-
+  <CCard>
+    <CCardHeader>
+      <h1>
+        Create Social Media
+      </h1>
+    </CCardHeader>
+    <CCardBody>
+      <div id="selectedContainer">
+        <CIcon :name="selected" id="selectedIcon" size="custom"/>
+      </div>
+      <div id="iconContainer">
+        <CButton class="iconButton" v-for="item in iconNames" :key="item" @click="clickIcon(item)" >
+          <CIcon :name="item" customClasses="icons" size="custom"/>
+        </CButton>
+      </div>
+    </CCardBody>
+  </CCard>
 </template>
 
 <script>
+
+import {iconsSet} from "../assets/icons/icons";
+
 export default {
-  name: "CreateSocial"
+  name: "CreateSocial",
+  data() {
+    return {
+      selected: ''
+    }
+  },
+  mounted() {
+    console.log(Object.keys(iconsSet))
+  },
+  computed: {
+    iconNames() {
+      return Object.keys(iconsSet)
+    }
+  },
+  methods: {
+    clickIcon(item) {
+      this.selected = item
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+#iconContainer {
+  background: #ddd;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px;
+  border-radius: 10px;
+  .iconButton {
+    flex: 0 0 40px;
+    margin: 5px;
+    padding: 0px;
+  }
+}
 
+
+
+#selectedContainer {
+  background: #aaa;
+  border-radius: 10px;
+  width: 50px;
+  height: 50px;
+  margin-bottom: 10px;
+  padding: 5px;
+  .selectedIcon {
+
+  }
+}
 </style>
