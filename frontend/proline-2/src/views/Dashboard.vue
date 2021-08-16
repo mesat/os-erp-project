eae86aa629baa9396c959a2f69fdc40b58e49620
mbsoylu <soylumervebeyza@gmail.com>
2021-08-16 13:36:44 +0300

    dashboard kısmı düzenlendi.

--- frontend/proline-2/src/views/Dashboard.vue
+++ frontend/proline-2/src/views/Dashboard.vue
@@ -6,18 +6,23 @@
         <CRow>
 
           <CCol col="6">
-            <CJumbotron>
+            <CJumbotron class="h-75">
             <CCardText id="header">
           <h1>PROLİNE BİLİŞİM</h1>
+
             </CCardText>
             </CJumbotron>
           </CCol>
 
 
               <CCol col="6">
-                <CJumbotron>
-                <CCardText>
-                  <h1>PROLİNE BİLİŞİM</h1>
+                <CJumbotron class="h-75">
+                <CCardText >
+                  <h1 id="contactInformation">Contact Information</h1>
+
+                  Şerifali Mah. Beyan Sok. No:60/1 34775 Ümraniye - ISTANBUL<br>
+                  Tel  : +90 (216) 528 62 00<br>
+                  Faks: +90 (216) 528 63 00
                 </CCardText>
                  </CJumbotron>
               </CCol>
@@ -28,18 +33,35 @@
         <CRow>
 
           <CCol col="6">
-            <CJumbotron>
-              <CCardText id="header">
-                <h1>PROLİNE BİLİŞİM</h1>
+            <CJumbotron class="h-auto">
+              <CCardText >
+                <p1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Vestibulum accumsan condimentum enim a varius.
+                  Maecenas placerat mattis risus, quis elementum augue aliquet vel.
+                  Pellentesque id metus magna. Praesent iaculis diam quis odio dapibus,
+                  in elementum nulla ornare. Phasellus posuere eros nisi, in porta purus fermentum vitae.
+                  Quisque eu sem est. Nulla gravida tortor convallis posuere condimentum.
+                  Aliquam pulvinar ullamcorper gravida. Nullam sed erat dolor. Nunc finibus,
+                  elit dignissim fringilla lobortis, neque metus hendrerit urna,
+                  id aliquam ligula mauris id nunc.
+                </p1>
               </CCardText>
             </CJumbotron>
           </CCol>
 
 
           <CCol col="6">
-            <CJumbotron>
+            <CJumbotron class="h-auto">
               <CCardText>
-                <h1>PROLİNE BİLİŞİM</h1>
+                <p2>Maecenas semper efficitur tellus eget placerat.
+                  Nullam accumsan, justo nec feugiat pellentesque,
+                  ante dui pulvinar turpis, eu blandit velit augue ac risus. Nulla nec accumsan mauris,
+                  a malesuada eros. Curabitur neque odio, bibendum eu velit eu, luctus laoreet augue.
+                  Curabitur neque ligula, iaculis quis sem quis, vulputate pretium enim.
+                  Fusce consequat scelerisque dictum. Morbi tristique blandit facilisis.
+                  Quisque eu massa porttitor ex posuere finibus.
+                  Nam rhoncus, augue fermentum porta molestie, nulla nisl dignissim arcu,
+                  eu semper ante ipsum et purus. Aliquam aliquam odio eu finibus feugiat.</p2>
               </CCardText>
             </CJumbotron>
           </CCol>
@@ -59,11 +81,20 @@ export default {
 </script>
 
 <style scoped>
-#header{
+#header {
   color: #42b983;
-  text-shadow: 3px 3px darkblue;
+  text-shadow: 3px 3px #3c4b64;
   font-size: larger;
   font-family: "Segoe UI Black";
-
 }
+
+  #contactInformation{
+    color: #2c3e50;
+    text-shadow: 2px 2px #3c4b64;
+    font-size: xx-large;
+    font-family: "Segoe UI Black";
+
+  }
+
+
 </style>
