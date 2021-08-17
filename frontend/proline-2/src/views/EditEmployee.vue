d1d7c04e0504507925f36c246b63cd63638a9914
alperen010010 <bircakalperen@gmail.com>
2021-08-17 09:30:08 +0300

    Teamview created and edit employee validation

--- frontend/proline-2/src/views/EditEmployee.vue
+++ frontend/proline-2/src/views/EditEmployee.vue
@@ -18,7 +18,7 @@
                   name="name"
                   label="Name"
                   placeholder="Enter employees name"
-                  :invalidFeedback= "message.name"
+                  :invalidFeedback="message.name"
                   :isValid="isValidName"
                   v-model="name"
               />
@@ -28,9 +28,9 @@
                   name="last-name"
                   label="Surname"
                   placeholder="Enter employees surname"
-                  invalidFeedback= ""
+                  invalidFeedback=""
                   tooltipFeedback
-                  :isValid = "isValidName"
+                  :isValid="isValidName"
                   v-model="surName"
               />
             </CCol>
@@ -40,9 +40,9 @@
                     label="Date"
                     type="date"
                     v-model="date"
-                    invalidFeedback= "message.date"
+                    invalidFeedback="message.date"
                     tooltipFeedback
-                    :isValid = "isValidDate"
+                    :isValid="isValidDate"
                 />
               </CCol>
             </CRow>
@@ -52,9 +52,9 @@
                 name="role"
                 label="Role"
                 placeholder="Role in the company"
-                invalidFeedback= ""
+                invalidFeedback=""
                 tooltipFeedback
-                :isValid = "isValidRole"
+                :isValid="isValidRole"
                 v-model="role"
             />
           </CRow>
@@ -65,9 +65,9 @@
                   label="Bio"
                   rows="8"
                   placeholder="Short bio of the employee..."
-                  invalidFeedback= ""
+                  invalidFeedback=""
                   tooltipFeedback
-                  :isValid = "isValidBio"
+                  :isValid="isValidBio"
                   v-model="bio"
               />
             </CCol>
@@ -87,20 +87,20 @@
           label="Telephone Number"
           type="mail"
           placeholder="Telephone number"
-          :invalidFeedback= "message.tel"
+          :invalidFeedback="message.tel"
           v-model="tel"
           tooltipFeedback
-          :isValid = "isValidTel"
+          :isValid="isValidTel"
       />
       <CInput
           name="e-mail"
           label="E-Mail"
           type="tel"
           placeholder="Enter mail"
-          invalidFeedback= ""
+          invalidFeedback=""
           tooltipFeedback
           v-model="mail"
-          :isValid = "isValidMail"
+          :isValid="isValidMail"
       />
     </CCardBody>
     <CCardHeader>
@@ -112,15 +112,15 @@
     <CCardFooter>
       <CRow>
         <CCol col="0" md="6">
-          <CAlert :color="notificationColor" close-button  :show.sync="showError">
+          <CAlert :color="notificationColor" close-button :show.sync="showError">
             {{warning}}
           </CAlert>
         </CCol>
         <CCol md="2">
-          <CButton block color="danger" @click="deleteClick" >Delete</CButton>
+          <CButton block color="danger" @click="deleteClick">Delete</CButton>
         </CCol>
         <CCol md="2">
-          <CButton block color="warning" @click="cancel" >Cancel</CButton>
+          <CButton block color="warning" @click="cancel">Cancel</CButton>
         </CCol>
         <CCol md="2">
           <CSpinner color="success" v-if="loading"/>
@@ -136,6 +136,7 @@ import Socials from "../components/Socials";
 import {axios} from "../javascript/_axios";
 import * as regex from "../javascript/_regex"
 import Employee from "../javascript/_employee";
+
 const urlCreator = window.URL || window.webkitURL;
 
 export default {
@@ -161,7 +162,7 @@ export default {
     }
   },
   created() {
-    this.message.name= regex.nameMsg
+    this.message.name = regex.nameMsg
     this.message.surName = regex.nameMsg
     this.message.date = regex.dateMsg
     this.message.role = regex.roleMsg
@@ -178,10 +179,14 @@ export default {
           this.date = item.date
           this.role = item.role
           this.bio = item.bio
-          this.mail = item.socials.find((a) =>{ return a.name==='MAIL'}).link
-          this.tel = item.socials.find((a) =>{ return a.name==='TEL_NO'}).link
+          this.mail = item.socials.find((a) => {
+            return a.name === 'MAIL'
+          }).link
+          this.tel = item.socials.find((a) => {
+            return a.name === 'TEL_NO'
+          }).link
           this.socials = item.socials.filter(function (a) {
-            return a.name!=='MAIL' && a.name!=='TEL_NO'
+            return a.name !== 'MAIL' && a.name !== 'TEL_NO'
           })
 
           this.loading = false
@@ -192,93 +197,114 @@ export default {
   },
   methods: {
     submit() {
-      this.loading = true
-      let data = {
-        "name": this.name,
-        "surname": this.surName,
-        "rol": this.role,
-        "bio": this.bio,
-        "contactsById": [
-          {
-            "link": this.tel,
-            "socialmediaBySocialmediaPlatform": {
-              "platform": "TEL_NO",
+      if (
+          this.isValidName(this.name) &&
+          this.isValidName(this.surName) &&
+          this.isValidRole(this.role) &&
+          this.isValidMail(this.mail) &&
+          this.isValidTel(this.tel) &&
+          this.isValidBio(this.bio) &&
+          this.isValidDate(this.date)
+      ) {
+        this.loading = true
+        let data = {
+          "name": this.name,
+          "surname": this.surName,
+          "rol": this.role,
+          "bio": this.bio,
+          "contactsById": [
+            {
+              "link": this.tel,
+              "socialmediaBySocialmediaPlatform": {
+                "platform": "TEL_NO",
+              }
+            },
+            {
+              "link": this.mail,
+              "socialmediaBySocialmediaPlatform": {
+                "platform": "MAIL"
+              }
             }
-          },
-          {
-            "link": this.mail,
-            "socialmediaBySocialmediaPlatform": {
-              "platform": "MAIL"
+          ].concat(this.socials.map(function (a) {
+            return {
+              "link": a.link,
+              "socialmediaBySocialmediaPlatform": {
+                "platform": a.name
+              }
             }
-          }
-        ].concat(this.socials.map(function (a) {
-          return {
-            "link": a.link,
-            "socialmediaBySocialmediaPlatform": {
-              "platform": a.name
-            }
-          }
-        }))
-      }
-      console.log(data);
-      axios.request({
-        url: '/employees/' + this.$route.params.id,
-        method: "PUT",
-        data: data,
-        headers: {
-          'Content-Type': 'application/json'
+          }))
         }
-      }).then(() => {
-        this.showSuccess('Successfully updated Employee!')
-        this.loading = false
-      }).catch((error) => {
-        this.showAlert(error)
-        this.loading = false
-      })
-    },
-    cancel(){
+        console.log(data);
+        axios.request({
+          url: '/employees/' + this.$route.params.id,
+          method: "PUT",
+          data: data,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(() => {
+          this.showSuccess('Successfully updated Employee!')
+          this.loading = false
+        }).catch((error) => {
+          this.showAlert(error)
+          this.loading = false
+        })
+      } else {
+        this.showAlert("Invalid fields")
+      }
+    }
+    ,
+    cancel() {
       window.history.back()
-    },
-    deleteClick(){
-      axios.delete("/employees/"+this.$route.params.id)
+    }
+    ,
+    deleteClick() {
+      axios.delete("/employees/" + this.$route.params.id)
       window.history.back()
     }
     ,
     isValid(val, regex) {
-      if(val === '' || val === undefined)
-      {
+      if (val === '' || val === undefined) {
         return null
-      }
-      else return val.match(regex) !== null;
-    },
+      } else return val.match(regex) !== null;
+    }
+    ,
     isValidDate(val) {
       return this.isValid(val, regex.dateRegex)
-    },
+    }
+    ,
     isValidRole(val) {
       return this.isValid(val, regex.roleRegex)
-    },
+    }
+    ,
     isValidBio(val) {
       return this.isValid(val, regex.bioRegex)
-    },
+    }
+    ,
     isValidMail(val) {
       return this.isValid(val, regex.emailRegex)
-    },
+    }
+    ,
     isValidTel(val) {
       return this.isValid(val, regex.telRegex)
-    },
+    }
+    ,
     isValidName(val) {
       return this.isValid(val, regex.nameRegex)
-    },
+    }
+    ,
     showAlert(text) {
       this.notificationColor = "danger"
       this.showError = true
       this.warning = text
-    },
+    }
+    ,
     showSuccess(text) {
       this.notificationColor = "success"
       this.showError = true
       this.warning = text
-    },
+    }
+    ,
     fileChange(files, event) {
       this.uploaded = true
       this.imgsrc = urlCreator.createObjectURL(files[0])
